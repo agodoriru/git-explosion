3dd1c74da5ed514ff4b6c460e0cb307738b001c6
agodoriru <takumijohn0806@gmail.com>
2018-05-12 23:45:50 +0900

    NEW test

--- test_1/main.cpp
+++ test_1/main.cpp
@@ -4,14 +4,20 @@ using namespace std;
 
 int main()
 {
-	int a;
-	cin>>a;
+	
 
+	
 <<<<<<< HEAD
 	
 =======
 	cout<<a*3<<endl;
+<<<<<<< HEAD
 	cout<<a*2<<endl;
 >>>>>>> b5668f5... [fix] main.cpp
+=======
+	cout<<a*10<<endl;
+
+	cout<<a*a<<endl;
+>>>>>>> 59fd922... test2
 	return 0;
 }

37ccb01cb15bb0bbb5004382e4350380614b7ade
agodoriru <takumijohn0806@gmail.com>
2018-05-12 23:45:50 +0900

    test2

--- test_1/main.cpp
+++ test_1/main.cpp
@@ -11,7 +11,13 @@ int main()
 	
 =======
 	cout<<a*3<<endl;
+<<<<<<< HEAD
 	cout<<a*2<<endl;
 >>>>>>> b5668f5... [fix] main.cpp
+=======
+	cout<<a*10<<endl;
+
+	cout<<a*a<<endl;
+>>>>>>> 59fd922... test2
 	return 0;
 }

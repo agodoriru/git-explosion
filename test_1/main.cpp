6928b025329c9893ba7111470065924e633a9f96
agodoriru <takumijohn0806@gmail.com>
2018-05-13 00:48:22 +0900

    [add] Hello

--- test_1/main.cpp
+++ test_1/main.cpp
@@ -4,19 +4,6 @@ using namespace std;
 
 int main()
 {
-	
-
-
-	
-<<<<<<< HEAD
-	
-=======
-/<<<<<<< HEAD
-	cout<<a*2<<endl;
->>>>>>> b5668f5... [fix] main.cpp
-=======
-
-	cout<<a*a<<endl;
->>>>>>> 59fd922... test2
-	return 0;
+	cout<<"         "<<endl;
+	cout<<"Hello"<<endl;
 }
